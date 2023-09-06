93602a1215c96384b2872caf168b7dfd7baf9bcf
Jihee Kim <kimjihee1113@gmail.com>
2023-09-06 11:33:15 +0900

    파일분할 & 초기설정

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import './style/App.css'
+import Route from "./router/Route.jsx";
+
+function App() {
+  return (
+    <>
+      <Route/>
+    </>
+  )
+}
+
+export default App
---
+++ dashboard/src/router/Route.jsx
@@ -0,0 +1,8 @@
+function Route(){
+    return(
+        <div>
+            ddd
+        </div>
+    )
+}
+export default Route;
